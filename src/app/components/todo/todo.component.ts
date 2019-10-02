0aca632a8670a029c33110c3050cd6a6e81a968d
Raunak <sharmaraunak338@gmail.com>
2019-10-02 14:40:10 +0530

    angular todo list
    It is an angular to do list based on learnings fro crash course

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
+import { Todo } from '../../models/Todo'
+import { log } from 'util';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todos:Todo[];
+
+
+  constructor(private todoService:TodoService) {
+
+   }
+
+  ngOnInit() {
+   this.todoService.getTodos().subscribe(todos =>{
+     this.todos = todos;
+   });
+    
+  }
+
+  deleteTodo(todo:Todo){
+   
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe()
+
+  }
+
+  addTodo(todo:Todo)
+  {
+      this.todoService.addTodo(todo).subscribe(todo =>{
+        this.todos.push(todo);
+      });
+  }
+
+}
